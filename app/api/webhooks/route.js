3c5fc5e7cc61c94bb642b335643e1c3078cc5099
engmoha1717 <engmoha1717@gmail.com>
2024-09-14 04:29:01 -0700

    third

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -1,7 +1,10 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
-import { createUser } from '../../lib/user'
+// import { createUser } from '../../lib/user'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+
+import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
