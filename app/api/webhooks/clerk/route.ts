db25414ba5d1a79d29e970a8281758a7de670c4b
engmoha1717 <engmoha1717@gmail.com>
2024-09-15 07:48:26 -0700

    ss

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -5,51 +5,50 @@ import {createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
   if (!WEBHOOK_SECRET) {
-    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
 
+  // Get the headers
   const headerPayload = headers();
-  const svix_id = headerPayload.get('svix-id');
-  const svix_timestamp = headerPayload.get('svix-timestamp');
-  const svix_signature = headerPayload.get('svix-signature');
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
 
-  console.log('Received headers:', { svix_id, svix_timestamp, svix_signature });
-
+  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers', { status: 400 });
+    return new Response('Error occured -- no svix headers', {
+      status: 400
+    })
   }
 
-  const payload = await req.json();
-  console.log('Received payload:', payload);
-
+  // Get the body
+  const payload = await req.json()
   const body = JSON.stringify(payload);
+
+  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt: WebhookEvent;
+  let evt: WebhookEvent
 
-  console.log('Received headers:', {
-    svix_id,
-    svix_timestamp,
-    svix_signature
-  });
-  console.log('Received payload:', payload);
-  
-
+  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
-      'svix-id': svix_id,
-      'svix-timestamp': svix_timestamp,
-      'svix-signature': svix_signature,
-    }) as WebhookEvent;
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent
   } catch (err) {
     console.error('Error verifying webhook:', err);
-    return new Response('Error occured', { status: 400 });
+    return new Response('Error occured', {
+      status: 400
+    })
   }
 
-  console.log('Event verified, proceeding to handle:', evt.type);
+
+  
 
   const eventType = evt.type;
   console.log('Received event type:', eventType);
