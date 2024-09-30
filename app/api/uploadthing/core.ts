e736ec803ab9af17cb46dd478cc02cb9413e9c68
engmoha1717 <engmoha1717@gmail.com>
2024-09-30 02:44:53 -0700

    mmmm

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -26,7 +26,7 @@ export const ourFileRouter = {
 
       console.log("file url", file.url);
 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      
       return { uploadedBy: metadata.userId };
     }),
 } satisfies FileRouter;
